59ed69a932f8d1aad907d52a58d259f933d400d2
Ahmed Hassani <ah.jr22@gmail.com>
2020-08-17 17:59:40 +0300

    first int

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -29,7 +29,7 @@ public class Controller {
         }
         stegDym.write(inputStream,bytesFiles,2,pass,nameFile);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(stegDym.getStegImg(), "PNG", baos);
+        ImageIO.write(stegDym.getStegImg(), "png", baos);
         return baos.toByteArray();
     }
 
